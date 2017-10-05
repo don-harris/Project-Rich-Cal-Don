7a5c6ea675a8a4f1f08b540fbf00364fa27dff10
Octavia <octaviapalmer@hotmail.com>
2017-10-05 18:35:51 +1300

    data started

---
+++ seeds/places.js
@@ -0,0 +1,36 @@
+const places = {
+      [
+        {
+          id: 1001,
+          placeName: 'Fidel\'s Cafe',
+          url: ,
+          img: ,
+          lat: -41.2961479,
+          long: 174.7720173 
+        },
+        {
+          id: 1002,
+          placeName: 'Ekim Burgers',
+          url: ,
+          img: ,
+          lat: -41.2961479,
+          long: 174.7720173 
+        },
+        {
+          id: 1003,
+          placeName: 'Bristol',
+          url: ,
+          img: ,
+          lat: -41.2961479,
+          long: 174.7720173
+        },
+        {
+          id: 1004,
+          placeName: 'Subway (Eat Fresh)',
+          url: ,
+          img: ,
+          lat: -41.2961479,
+          long: 174.7720173
+        },
+      ]
+    }
---
+++ seeds/users.js
@@ -0,0 +1,121 @@
+const users = {
+      [
+        {
+          id: 1001,
+          name: 'Callan',
+          github: 'callan-stretton',
+          usrimage: '/images/cal.png',
+          alias: 'Callan the Careless Whisper',
+          superpower: 'He will whisper sweet code into your ear and comatose you into a deep deep slumber',
+          backStory: 'From the isles of Miramar'
+        },
+        {
+          id: 1002,
+          name: 'Raj',
+          github: 'rajal-kumar',
+          usrimage: '/images/raj.jpg',
+          alias: 'The Fijian Taxi Man',
+          superpower: 'Can teleport people to other places in milliseconds',
+          backStory: 'A mix of Indian and Fijian heritage, The Taxi man has aquired the skills to deal with his enemies with strength and hardwork'
+        },
+        {
+          id: 1003,
+          name: 'Joe',
+          github: 'JosephJvB',
+          usrimage: '/images/joe.jpg',
+          alias: 'Joey Badass',
+          superpower: 'Spits hot fire as he raps.',
+          backStory: "Now this is the story all about how my life got flipped, turned upside down and I'd like to take a minute if I may I'll tell you how I became the prince of a town called EDA"
+        },
+        {
+          id: 1004,
+          name: 'Richard',
+          github: 'richardjohns',
+          usrimage: '/images/rich.jpg',
+          alias: 'Long John Silver',
+          superpower: 'Creates long johns out of thin air which will suffocate his opponent',
+          backStory: 'Born on the backstreets in the biggest and toughest accounting city; The city of Compound Interest, located on planet dividends. Raised to be as brutal with accounts payable as with long johns, a hero to be respected and feared'
+        },
+        {
+          id: 1005,
+          name: 'Don',
+          github: 'don-smith',
+          usrimage: '/images/don.jpeg',
+          alias: 'The Code Thief',
+          superpower: 'Can take away your superpower by hacking your DNA with code',
+          backStory: 'Once a star pupil of the EDA land, he turned Rogue and became the heroes deadliest code destroyer'
+        },
+        {
+          id: 1006,
+          name: 'Harrison',
+          github: 'harrison-symes',
+          usrimage: '/images/harry.jpeg',
+          alias: 'Ladybug Killer',
+          superpower: 'Can turn into a lady bug but will go around stabbing people',
+          backStory: "sfdgsdgsdf help me I've been stabbed by a hairy ladybug! Ah well if you can't beat em join em..."
+        },
+        {
+          id: 1007,
+          name: 'Skye',
+          github: 'SkyeBelinda',
+          usrimage: '/images/skye.jpeg',
+          alias: 'The Sloth of the Skies',
+          superpower: 'Relaxed but deadly, she can fly while sleeping',
+          backStory: "It's a bird!.. it's a plane!... it's! zzzzzzzzzzzzzzzzz..."
+        },
+        {
+          id: 1008,
+          name: 'Aaron',
+          github: 'aaron-macdonald',
+          usrimage: '/images/aaron.png',
+          alias: 'The Coach',
+          superpower: 'Super at softball, the Coach does not play around. He will fire a thousand softballs at you before you can say THE SERVER IS DOWN',
+          backStory: 'Coaching all his life'
+        },
+        {
+          id: 1009,
+          name: 'Andrea',
+          github: 'amassignani',
+          usrimage: '/images/andrea.jpeg',
+          alias: 'The Bombastic Brazilian',
+          superpower: 'Her joyous laugh makes her enemies spin and into a state of calm',
+          backStory: 'Born in the heart of Brazil, taught to fight crime and have a good time'
+        },
+        {
+          id: 1010,
+          name: 'Octavia',
+          github: 'palmeroctavia',
+          usrimage: '/images/oct.jpeg',
+          alias: 'OctoLady',
+          superpower: 'OctoLady can multi-punch like no other being, she overwhelms her enemies with using too many attacks on them at once',
+          backStory: 'Talking to octopi all her life she has developed her skills to use her arms as 8 limbs rather than 2.'
+        },
+        {
+          id: 1011,
+          name: 'Daz',
+          github: 'dazyang',
+          usrimage: '/images/daz.jpeg',
+          alias: 'Freeform',
+          superpower: 'Turns into water so she can drown her enemies.',
+          backStory: 'Oh hi Daz how was your weeke- agh!blugglugblahhhhcroooak'
+        },
+        {
+          id: 1012,
+          name: 'Annah',
+          github: 'AnnahGerletti',
+          usrimage: '/images/annah.jpeg',
+          alias: 'Wonder Woman',
+          superpower: 'Clone myself',
+          backStory: "What's better than one Wonder Woman?? Well two of course..."
+        },
+        {
+          id: 1013,
+          name: 'Don H',
+          github: 'don-harris',
+          usrimage: '/images/donh.jpeg',
+          alias: 'The Murricane',
+          superpower: 'Keeps living the same day over... and over... and over... and over... and plays a mean piano',
+          backStory: 'This fearsome warrior trained for thousands of years to master the little-known martial art of Murri-foo. Avoid triggerwords such as Zombieland, and Groundhog Day'
+        }
+      ]
+    }
