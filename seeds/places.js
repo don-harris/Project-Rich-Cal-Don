796ad677cce052a42fefd6396c7741343035f9af
Octavia <octaviapalmer@hotmail.com>
2017-10-05 19:13:34 +1300

    further data updates

--- seeds/places.js
+++ seeds/places.js
@@ -3,34 +3,34 @@ const places = {
         {
           id: 1001,
           placeName: 'Fidel\'s Cafe',
-          url: ,
-          img: ,
+          url: 'http://www.fidelscafe.com/',
+          img: 'https://www.bloggeratlarge.com/wp-content/uploads/Fidels-Cuba-St.jpg',
           lat: -41.2961479,
           long: 174.7720173 
         },
         {
           id: 1002,
           placeName: 'Ekim Burgers',
-          url: ,
-          img: ,
-          lat: -41.2961479,
-          long: 174.7720173 
+          url: 'https://www.zomato.com/wellington/ekim-burgers-te-aro-wellington-city',
+          img: 'http://whatsgood.co.nz/wp-content/uploads/2015/03/ekim-menu.jpg',
+          lat: -41.2962865,
+          long: 174.7742274 
         },
         {
           id: 1003,
-          placeName: 'Bristol',
-          url: ,
-          img: ,
-          lat: -41.2961479,
-          long: 174.7720173
+          placeName: 'Hotel Bristol',
+          url: 'https://www.hotelbristol.co.nz/',
+          img: 'http://www.wellingtoncityheritage.org.nz/~/media/heritage/buildings/0-150/078-4/image5.ashx?mw=671&mh=415',
+          lat: -41.2930679,
+          long: 174.7759682
         },
         {
           id: 1004,
           placeName: 'Subway (Eat Fresh)',
-          url: ,
-          img: ,
-          lat: -41.2961479,
-          long: 174.7720173
+          url: 'https://www.zomato.com/wellington/subway-1-te-aro-wellington-city',
+          img: 'https://bayleys-pri-cdn-endpoint.azureedge.net/listing-images/1950839/1950839_2_Listing_v2.jpg?v=636367079995900000',
+          lat: -41.2949653,
+          long: 174.7754525
         },
       ]
     }
