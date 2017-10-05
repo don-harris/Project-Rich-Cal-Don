b271420be7a4df840963c62e019134355ca5ff97
dazyang <dazyang.contact@gmail.com>
2017-10-06 10:14:11 +1300

    broken map

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,30 +1,19 @@
 import React from 'react'
-import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps' // import sub-modules from library the library, not the whole thing
+import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps' // import sub-modules from the library, not the whole thing
 
 class Map extends React.Component {
-
-  const center = {
-    lat: - 41.296772,
-    lng: 174.773787
-  }
   constructor(props) {
     super(props)
-    this.state = {center: center}
+    this.state = {center: props.center}
   }
 
   render () {
     const mapContainer = <div style={{height: '100%', width: '100%'}}></div> // Map fill entire contents of its parent
-
+    // return mapContainer
     return (
       <GoogleMapLoader
         containerElement = { mapContainer }
-        googleMapElement = {
-          <GoogleMap
-            defaultZoom={15}
-            defaultCenter={this.props.center}
-            options={{streetViewControl: false, mapTypeControl: false}}>
-          </GoogleMap>
-        } />
+      />
     )
   }
 }
--- client/components/View2.jsx
+++ client/components/View2.jsx
@@ -1,31 +1,65 @@
 import React from 'react'
 
-//
-
 import Map from './Map'
 
-class View2 extends React.Component {
-  constructor (props){
-    super(props)
-    this.state = {
-
-    }
-  }
-  render() {
-    const location = { // location variable, passing this as a center property to the Map ∨∨∨ 
+const View2 = () => {
+  const location = {
       lat: - 41.296772,
       lng: 174.773787
-    }
+     }
+      // location variable, passing this as a center property to the Map ∨∨∨ 
 
-    return (
-      <div className='mapContainerDiv'>
-        <h2>Map will go below this</h2>
-        <div style={{ width: 300, height: 600, background: 'red' }}>
-          <Map center={location} />
-        </div>
-      </div >
-    )
-  } // setting up size of div that map goes in ^^^
-}
+  return (
+    <div className='mapContainerDiv'>
+      <h2>Map will go below this</h2>
+      <div style={{ width: 300, height: 600, background: 'red' }}>
+        <Map center={location} />
+      </div>
+    </div >
+  )
+} // setting up size of div that map goes in ^^^
+
 
 export default View2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class View2 extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+
+//     }
+//   }
+//   render() {
+//     const location = { // location variable, passing this as a center property to the Map ∨∨∨ 
+//       lat: - 41.296772,
+//       lng: 174.773787
+//     }
+
+//     return (
+//       <div className='mapContainerDiv'>
+//         <h2>Map will go below this</h2>
+//         <div style={{ width: 300, height: 600, background: 'red' }}>
+//           <Map center={location} />
+//         </div>
+//       </div >
+//     )
+//   } // setting up size of div that map goes in ^^^
+// }
