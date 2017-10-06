8041c14aca0d966012293a370b3a4f6c26b1fa43
dazyang <dazyang.contact@gmail.com>
2017-10-06 15:11:12 +1300

    map layout changes

--- client/components/View2(old).jsx
+++ client/components/View2(old).jsx
@@ -11,35 +11,27 @@ const View2 = (props) => {
       <h1>{props.shoutee.name} is gonna shout stuff</h1>
       <div style={{ width: '40vh', height: '60vh', background: 'black' }}>
         <Map center={props.places.location} />
-      </div>
+      </div> */}
 
       <div className="card">
         <div className="card-image">
-          <figure className="image is-4by3">
-            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+          <figure className="image is-square">
+            <img src={props.places.img} alt="Placeholder image">
           </figure>
         </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-left">
-              <figure className="image is-48x48">
-                <img src="http://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
-              </figure>
-            </div>
-            <div className="media-content">
-              <p className="title is-4">John Smith</p>
-              <p className="subtitle is-6">@johnsmith</p>
-            </div>
-          </div>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+              </div>
+                <div className="media-content">
+                <p className="title is-4">{props.places.placeName}</p>
+                </div>
+              </div>
 
-          <div className="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-            <a href="#">#css</a> <a href="#">#responsive</a>
-            <br/>
-            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              <div className="content">
+              <a href={props.places.url}>Website</a>.
+              </div>
           </div>
-        </div>
       </div>
 
       <button onClick={props.closeMap}>Close</button>
